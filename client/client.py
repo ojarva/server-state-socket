89dcc61cfa71265b5fb6c94e270bd0549a200955
Olli Jarva <olli.jarva@futurice.com>
2016-03-10 22:30:51 +0200

    Include ping, timeouts and robust logging

--- client/client.py
+++ client/client.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
-from local_settings import PORT, SERVER_IP
+import datetime
 import json
+import logging
 import multiprocessing
 import redis
+import select
 import socket
 import sys
 import time
@@ -11,25 +13,114 @@ import uuid
 
 
 class StateClient(object):
+    STATES = {
+        "connected": 2,
+        "connected_before_reply": 1,
+        "disconnected": 0,
+        "unknown": -1,
+    }
+
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
+
+        self.logger = logging.getLogger("socket-state-client")
+        self.logger.setLevel(logging.INFO)
+        format_string = "%(asctime)s - {server_ip}:{server_port} - %(levelname)s - %(message)s".format(server_ip=self.ip, server_port=self.port)
+        formatter = logging.Formatter(format_string)
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
+
         self.redis = redis.StrictRedis()
+        self.has_connected = None
+        self.last_sent_at = 0
+        self.current_reconnect_time = 0.5
 
     def connect_and_ping(self):
         s = socket.create_connection((self.ip, self.port), 5)
+        self.send_influx_state("connected_before_reply")
         while True:
             ping_data = str(uuid.uuid4())
-            s.send("ping %s" % ping_data)
-            data = s.recv(1024)
-            if data.startswith("pong"):
-                if data[5:] == ping_data:
-                    self.send_status("running")
+            sent_at = time.time()
+            s.send("ping {random_data} {timestamp:.5f}".format(random_data=ping_data, timestamp=sent_at))
+            self.logger.debug("Sent ping {random_data} {timestamp:.5f}".format(random_data=ping_data, timestamp=sent_at))
+            ready = select.select([s], [], [], 5)
+            if ready[0]:
+                data = s.recv(1024)
+                self.logger.debug("Received {data}".format(data=data))
+            else:
+                self.logger.info("Connected, but no reply from server.")
+                s.close()
+                raise socket.error
+            if data.lower().startswith("pong"):
+                reply_received_at = time.time()
+                received_data = data.split(" ")
+                if len(received_data) == 3:
+                    if received_data[1] == ping_data:
+                        if self.has_connected is False or self.has_connected is None:
+                            self.current_reconnect_time = 0.5
+                            self.send_influx_state("connected")
+                            self.has_connected = True
+                        server_received_at = float(received_data[2])
+                        client_to_server = server_received_at - sent_at
+                        server_to_client = reply_received_at - server_received_at
+                        if client_to_server < 0:
+                            self.logger.warning("client->server timestamp is <0: {client_to_server}".format(client_to_server=client_to_server))
+                        if server_to_client < 0:
+                            self.logger.warning("server->client timestamp is <0: {server_to_client}".format(server_to_client=server_to_client))
+
+                        if time.time() - self.last_sent_at > 10 and client_to_server > 0 and server_to_client > 0:
+                            self.send_influx_values(client_to_server, server_to_client)
+                            self.logger.debug("Sent at {sent_at:.5f}, server received at {server_received_at:.5f}, finished at {reply_received_at:.5f}. Client->server: {client_to_server:.5f}. Server->client: {server_to_client:.5f}".format(sent_at=sent_at, server_received_at=server_received_at, reply_received_at=reply_received_at, client_to_server=client_to_server, server_to_client=server_to_client))
+                        self.send_status("running")
+                else:
+                    self.logger.info("Received malformed reply: {reply}".format(reply=data))
+            else:
+                self.logger.info("Received invalid reply: {reply}".format(reply=data))
             time.sleep(1)
         s.close()
 
+    def send_influx_values(self, client_to_server, server_to_client):
+        client_to_server = round(client_to_server, 6)
+        server_to_client = round(server_to_client, 6)
+        self.logger.debug("Sending values to influx: client->server: {client_to_server}, server_to_client: {server_to_client}".format(client_to_server=client_to_server, server_to_client=server_to_client))
+        self.last_sent_at = time.time()
+        influx_data = [{
+            "measurement": "socket-ping",
+            "tags": {
+                "host": self.ip,
+                "port": self.port,
+            },
+            "time": datetime.datetime.utcnow().isoformat() + "Z",
+            "fields": {
+                "client_to_server": client_to_server,
+                "server_to_client": server_to_client,
+            },
+        }]
+        self.redis.publish("influx-update-pubsub", json.dumps(influx_data))
+
     def send_status(self, status):
+        self.logger.debug("Broadcasting server status: {status}".format(status=status))
         self.redis.publish("home:broadcast:generic", json.dumps({"key": "server_power", "content": {"status": status}}))
+
+    def send_influx_state(self, state):
+        timestamp = datetime.datetime.utcnow().isoformat() + "Z"
+        state_number = self.STATES.get(state)
+        self.logger.info("State changed to {state} at {timestamp}".format(state=state, timestamp=timestamp))
+        influx_data = [{
+            "measurement": "internet-connection-stability",
+            "tags": {
+                "host": self.ip,
+                "port": self.port,
+            },
+            "time": timestamp,
+            "fields": {
+                "state": state,
+                "state_number": state_number,
+            },
+        }]
+        self.redis.publish("influx-update-pubsub", json.dumps(influx_data))
 
     def run(self):
         while True:
@@ -37,13 +128,19 @@ class StateClient(object):
                 self.connect_and_ping()
             except socket.error:
                 self.send_status("down")
-            time.sleep(1)
+                if self.has_connected is True or self.has_connected is None:
+                    self.send_influx_state("disconnected")
+                    self.has_connected = False
+            self.logger.debug("Sleeping for {reconnect_time}".format(reconnect_time=self.current_reconnect_time))
+            time.sleep(self.current_reconnect_time)
+            self.current_reconnect_time = min(self.current_reconnect_time * 1.25, 10)
 
 
 def main(args):
     if len(args) == 2:
         ip, port = args[0], args[1]
     else:
+        from local_settings import PORT, SERVER_IP
         ip, port = SERVER_IP, PORT
     state_client = StateClient(SERVER_IP, PORT)
     state_client.run()
--- server/app.py
+++ server/app.py
@@ -1,21 +1,46 @@
 #!/usr/bin/env python
 
 from local_settings import PORT
-import SocketServer
 from threading import Thread
+import logging
+import SocketServer
+import time
+
+
+logger = logging.getLogger("socket-state-server")
+logger.setLevel(logging.INFO)
+format_string = "%(asctime)s - %(client_ip)s:%(client_port)s - %(levelname)s - %(message)s"
+formatter = logging.Formatter(format_string)
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 
 class service(SocketServer.BaseRequestHandler):
     def handle(self):
         data = 'dummy'
-        print "Client connected with ", self.client_address
+        client_ip, client_port = self.client_address
+        logging_extra = {"client_ip": client_ip, "client_port": client_port}
+        logger.info("Connected", extra=logging_extra)
         while len(data):
-            data = self.request.recv(1024)
+            data = self.request.recv(1024).strip()
             if len(data) > 0 and data.lower().startswith("ping"):
-                self.request.send("pong" + data[4:])
+                request_data = data.split(" ")
+                if len(request_data) == 3:
+                    request_id = request_data[1]
+                    request_timestamp = float(request_data[2])
+                    now = time.time()
+                    diff = now - request_timestamp
+                    logger.debug("Received {sent_timestamp:.5f} at {receive_timestamp:.5f}: {diff:.5f}".format(sent_timestamp=request_timestamp, receive_timestamp=now, diff=diff), extra=logging_extra)
+                    self.request.send("pong {request_id} {receive_timestamp:.5f}".format(request_id=request_id, receive_timestamp=now))
+                else:
+                    logger.info("Received malformed ping request: {data}".format(data=data), extra=logging_extra)
+                    self.request.send("Invalid request\n")
             else:
+                logger.info("Received invalid request: {data}".format(data=data), extra=logging_extra)
                 self.request.send("Invalid request\n")
 
-        print "Client exited"
+        logger.info("Disconnected", extra=logging_extra)
         self.request.close()
 
 
